6f1bd789dd011c50b88d0443dbec143f87992ed1
남동현 <ehdgus5178@gmail.com>
2026-05-23 16:34:09 +0900

    ReLU 활성화 함수 구현
    ReLU 활성화 함수 구현
    
    - np.maximum을 사용해 ReLU forward 구현
    - backward에서 사용할 activation mask 저장
    - 저장된 mask를 사용해 ReLU backward 구현
    - VS Code에서 src import 경로를 인식하도록 설정 추가
    - 로컬 학습 자료와 참고 레포를 gitignore에 추가

--- src/activations.py
+++ src/activations.py
@@ -6,7 +6,7 @@
 - ReLU.forward, ReLU.backward
 - Softmax.forward, Softmax.backward
 """
-
+# D:\miniconda3\condabin\conda.bat activate mnist-lab
 import numpy as np
 
 
@@ -27,7 +27,9 @@ class ReLU:
             x와 같은 shape. x > 0인 위치만 원래 값을 유지합니다.
         """
         # TODO: x > 0 위치를 self.mask에 저장하고, 음수/0 위치는 0으로 바꾸세요.
-        raise NotImplementedError("ReLU.forward를 구현하세요.")
+        self.mask = (x > 0)
+        return np.maximum(0,x)
+
 
     def backward(self, dout):
         """
@@ -38,7 +40,9 @@ class ReLU:
             ReLU 입력 x에 대한 gradient. forward 때 x <= 0이었던 위치는 0입니다.
         """
         # TODO: forward에서 저장한 self.mask를 이용해 gradient가 흐를 위치만 남기세요.
-        raise NotImplementedError("ReLU.backward를 구현하세요.")
+        dx = dout.copy()
+        dx[~self.mask] = 0
+        return dx
 
 
 class Softmax:
